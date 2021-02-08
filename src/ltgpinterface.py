dabb72cc0e1e1c5cc105891cc8174cd050255e81
John Moncrieff <J.Moncrieff@ed.ac.uk>
2021-02-08 10:38:23 +0000

    updated widgets

---
+++ src/ltgpinterface.py
@@ -0,0 +1,110 @@
+"""
+Created: Tuesday 1st December 2020
+@author: John Moncrieff (j.moncrieff@ed.ac.uk)
+Last Modified on 7 Feb 2021 22:51 
+
+DESCRIPTION
+===========
+This package contains the class object for configuring and running 
+the long term gaussian plume Jupyter notebook
+
+"""
+
+import ipywidgets as widgets
+from IPython.display import display
+
+class ltgpinterface():
+
+    def __init__(self):
+         # ADMS stability classes
+        self.stability = {
+            'A':  {'u*': 0.1, 'H': 350, "L":-2 },
+            'B':  {'u*': 0.2, 'H': 250, "L":-10 },
+            'C':  {'u*': 0.5, 'H': 150, "L":-100 },
+            'D':  {'u*': 0.5, 'H': 50, "L":0 },
+            'E':  {'u*': 0.3, 'H': 25, "L":100 },
+            'F':  {'u*': 0.2, 'H': -5, "L":20 },
+            'G':  {'u*': 0.1, 'H': -25, "L":5 },
+            }
+        
+        self.stack = {"wind": 3, 
+                         "wdirn": 225, 
+                         "stab":"D",
+                         "height": 10,
+                         "strength": 15,
+                         "heat": 10,
+                         "nstack": 1,
+                         "view": "plan"
+                    }
+        self.stabls = ["A","B","C","D","E","F","G"]
+        self.outp=['plan','time_series','height_slice']
+        self.bit_nstacks = widgets.BoundedIntText(value = 1, min=1,  max=10, step=1, 
+                                     description="N Stacks", width=50)
+        self.bit_wind = widgets.BoundedIntText(value = 2, min=1,  max=10, step=1, 
+                                     description="wind speed $m \ s^{-1}$", width=50)
+        self.bit_height = widgets.BoundedIntText(value = 10, min=1, max=75, step=1, 
+                                        description="Height (m)", width=50)
+        self.bit_strength = widgets.BoundedIntText(value =100,  min=15, max=400, step=10, 
+                                        description="source strength g s-1", width=50)
+        self.dd_stability = widgets.Dropdown(value ="A", options=self.stabls, 
+                                       description="stability", width=50)
+        self.dd_viewd = widgets.Dropdown(value ="plan", options=self.outp, 
+                                       description="views", width=50)
+        self.bit_wdirn = widgets.BoundedIntText(value=225, min=1, max=359, step=5, 
+                                       description="wind direction", width=50)
+        self.bit_heat = widgets.BoundedIntText(value=5, min=1, max=100, step=5, 
+                                       description="heat output", width=50)
+        
+        #self.sumtotal = widgets.Text(value=self.sumtt,description="Total should be 100%", width=50, color='red')
+        
+        self.bit_nstacks.observe(self.bit_nstacks_eventhandler, names='value')
+        self.bit_wind.observe(self.bit_wind_eventhandler, names='value')
+        self.bit_height.observe(self.bit_height_eventhandler, names='value')
+        self.bit_strength.observe(self.bit_strength_eventhandler, names='value')
+        self.dd_stability.observe(self.dd_stability_eventhandler, names='value')
+        self.dd_viewd.observe(self.dd_viewd_eventhandler, names='value')
+        self.bit_wdirn.observe(self.bit_wdirn_eventhandler, names='value')
+        self.bit_heat.observe(self.bit_heat_eventhandler, names='value')
+        
+        #self.btn = widgets.Button(description='Run RLINE', width=100)
+        #self.btn.style.button_color = 'tomato'
+        #self.btn.on_click(self.btn_eventhandler)
+        self.h1 = widgets.HBox(children=[self.bit_wind, self.bit_wdirn, self.dd_stability])
+        self.h2 = widgets.HBox(children=[self.bit_height, self.bit_strength, self.bit_heat])
+        self.h3 = widgets.HBox(children=[self.dd_viewd, self.bit_nstacks])
+        
+    def bit_nstacks_eventhandler(self,change):
+        self.bit_nstacks.observe(self.bit_nstacks_eventhandler, names='value')
+        self.stack["nstack"]=self.bit_nstacks.value
+           
+    def bit_wind_eventhandler(self,change):
+        self.bit_wind.observe(self.bit_wind_eventhandler, names='value')
+        self.stack["wind"]=self.bit_wind.value
+
+    def bit_wdirn_eventhandler(self,change):
+        self.bit_wdirn.observe(self.bit_wdirn_eventhandler, names='value')
+        self.stack["wdirn"]=self.bit_wdirn.value
+
+    def dd_stability_eventhandler(self,change):
+        self.dd_stability.observe(self.dd_stability_eventhandler, names='value')
+        self.stack["stab"]=self.dd_stability.value
+        
+    def dd_viewd_eventhandler(self,change):
+        self.dd_viewd.observe(self.dd_viewd_eventhandler, names='value')
+        self.stack["view"]=self.dd_viewd.value
+
+    def bit_height_eventhandler(self,change):
+        self.bit_height.observe(self.bit_height_eventhandler, names='value')
+        self.stack["height"]=self.bit_height.value
+
+    def bit_strength_eventhandler(self,change):
+        self.bit_strength.observe(self.bit_strength_eventhandler, names='value')
+        self.stack["strength"]=self.bit_strength.value
+
+    def bit_heat_eventhandler(self,change):
+        self.bit_heat.observe(self.bit_heat_eventhandler, names='value')
+        self.stack["heat"]=self.bit_heat.value
+
+  
+        
+        
---
+++ src/streetimg.py
@@ -0,0 +1,83 @@
+"""
+Class to produce a streetmap image
+# Get the Streetmap image for the background
+# Needs to be done ONCE at the start of the program
+# and put in memory, rather than be loaded from disk. 
+# https://bryanbrattlof.com/adding-openstreetmaps-to-matplotlib/
+
+"""
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import requests
+from itertools import product
+from io import BytesIO
+
+class stmap():
+    def __init__(self,lat_max,lat_min,lon_min,lon_max,zoom):
+
+        self.URL = "https://tile.openstreetmap.org/{z}/{x}/{y}.png".format
+        self.TILE_SIZE = 256
+        self.top, self.bot = lat_max, lat_min
+        self.lef, self.rgt = lon_min, lon_max
+        self.zoom = zoom   
+
+        
+    def point_to_pixels(self,lon, lat, zoom):
+        """convert gps coordinates to web mercator"""
+        self.r = math.pow(2, zoom) * self.TILE_SIZE
+        lat = math.radians(lat)
+        self.x = int((lon + 180.0) / 360.0 * self.r)
+        self.y = int((1.0 - math.log(math.tan(lat) + 
+           (1.0 / math.cos(lat))) / math.pi) / 2.0 * self.r)
+        return self.x, self.y
+
+
+    def drawmp(self): # Lets just check we've brought in the correct map!
+            plt.figure(figsize=(4, 4))
+            plt.axis('off')
+            plt.imshow(self.img, aspect='auto')
+            plt.show()
+        # img now stored in memory for later use. 
+
+        
+    def run(self):
+        self.x0, self.y0 = self.point_to_pixels(self.lef, self.top, self.zoom)
+        self.x1, self.y1 = self.point_to_pixels(self.rgt, self.bot, self.zoom)
+
+        self.x0_tile, self.y0_tile = int(self.x0 / self.TILE_SIZE), int(self.y0 / self.TILE_SIZE)
+        self.x1_tile, self.y1_tile = math.ceil(self.x1 / self.TILE_SIZE), math.ceil(self.y1 / self.TILE_SIZE)
+
+        assert (self.x1_tile - self.x0_tile) * (self.y1_tile - self.y0_tile) < 50, "That's too many tiles!"
+
+        # full size image we'll add tiles to
+        self.img = Image.new('RGB', (
+            (self.x1_tile - self.x0_tile) * self.TILE_SIZE,
+            (self.y1_tile - self.y0_tile) * self.TILE_SIZE))
+
+        # loop through every tile inside our bounded box
+        for self.x_tile, self.y_tile in product(range(self.x0_tile, self.x1_tile),
+                              range(self.y0_tile, self.y1_tile)):
+
+            with requests.get(self.URL(x=self.x_tile, y=self.y_tile, z=self.zoom)) as resp:
+                self.tile_img = Image.open(BytesIO(resp.content))
+
+            # add each tile to the full size image
+            self.img.paste(
+                im=self.tile_img,
+                box=((self.x_tile - self.x0_tile) * self.TILE_SIZE,\
+                     (self.y_tile - self.y0_tile) * self.TILE_SIZE))
+
+        # Cropping The Multi Tile
+        self.xc, self.yc = self.x0_tile * self.TILE_SIZE, self.y0_tile * self.TILE_SIZE
+
+        self.img = self.img.crop((
+            self.x0 - self.xc,  # left
+            self.y0 - self.yc,  # top
+            self.x1 - self.xc,  # right
+            self.y1 - self.yc)) # bottom
+
+        self.drawmp()
+    # end run    
